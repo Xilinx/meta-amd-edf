42777490062e37cb6f5f804fd2afca17ed2600de
Altaf Patel <Patel.AltafAbedali@amd.com>
2025-11-19 12:53:49 +0530

    u-boot: Adjust ramdisk address to avoid overlap
    Updated ramdisk address in amd_edf.h to prevent memory overlap,
    This adjustment ensures proper memory allocation
    for ramdisk and prevents boot failures.
    
    Signed-off-by: Altaf Patel <Patel.AltafAbedali@amd.com>
    Message-ID: <20251119072349.2272870-1-Patel.AltafAbedali@amd.com>
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

--- recipes-bsp/u-boot/versal-2ve-2vm/amd_edf.h
+++ recipes-bsp/u-boot/versal-2ve-2vm/amd_edf.h
@@ -12,7 +12,7 @@
 	"kernel_size_r=0x10000000\0" \
 	"kernel_comp_addr_r=0x30000000\0" \
 	"kernel_comp_size=0x3C00000\0" \
-	"ramdisk_addr_r=0x22100000\0" \
+	"ramdisk_addr_r=0x24000000\0" \
 	"script_size_f=0x80000\0"
 
 /* Initial environment variables */
